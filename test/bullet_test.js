aad7af0ef9eb30f27211ca5d51527750b964a8a8
dvberkel <daan.v.berkel.1980+git@gmail.com>
2014-01-12 14:45:13 +0100

    keep asteroids#tick functionality

--- test/bullet_test.js
+++ test/bullet_test.js
@@ -3,6 +3,14 @@ var expect = require('chai').expect;
 var Bullet = require('../lib/bullet');
 
 describe('Bullet', function(){
+    var bullet;
+
+    beforeEach(function(){
+	bullet = new Bullet(function(){
+	    this.velocity({ speed : 1, heading: 0 });
+	});
+    });
+
     it('should exist', function(){
 	expect(Bullet).to.exist;
     });
@@ -13,5 +21,20 @@ describe('Bullet', function(){
 
     it('should be an instanceof Asteroid', function(){
 	expect(new Bullet()).to.be.an.instanceof(require('asteroids-asteroid'));
+    });
+
+
+    it('should have created a bullet', function(){
+	expect(bullet).to.exist;
+    });
+
+    describe('interactions', function(){
+	describe('tick', function(){
+	    it('should update position', function(){
+		bullet.tick();
+
+		expect(bullet.position()).to.deep.equal({ x: 1, y: 0 });
+	    });
+	});
     });
 });
