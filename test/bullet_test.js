de6871303d91e1d8c44804640d62f2f9c092a1ac
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2014-06-24 13:59:20 +0200

    fix indentation of bullet & bullet_test

--- test/bullet_test.js
+++ test/bullet_test.js
@@ -7,70 +7,70 @@ describe('Bullet', function(){
     var bullet;
 
     beforeEach(function(){
-	bullet = new Bullet(function(){
-	    this.velocity(options);
-	    this.ttl(options.lifetime);
-	});
+        bullet = new Bullet(function(){
+            this.velocity(options);
+            this.ttl(options.lifetime);
+        });
     });
 
     it('should exist', function(){
-	expect(Bullet).to.exist;
+        expect(Bullet).to.exist;
     });
 
     it('should be a constructor', function(){
-	expect(Bullet).to.be.a('function');
+        expect(Bullet).to.be.a('function');
     });
 
     it('should be an instanceof Asteroid', function(){
-	expect(new Bullet()).to.be.an.instanceof(require('asteroids-asteroid'));
+        expect(new Bullet()).to.be.an.instanceof(require('asteroids-asteroid'));
     });
 
 
     it('should have created a bullet', function(){
-	expect(bullet).to.exist;
+        expect(bullet).to.exist;
     });
 
     describe('object', function(){
-	['ttl'].forEach(function(method){
-	    it('should respond to the method ' + method, function(){
-		expect(bullet).to.respondTo(method);
-	    });
-	})
+        ['ttl'].forEach(function(method){
+            it('should respond to the method ' + method, function(){
+            expect(bullet).to.respondTo(method);
+            });
+        })
     });
 
     describe('interactions', function(){
-	describe('ttl', function(){
-	    it('should set timeToDeath', function(){
-		var lifetime = 3;
-		bullet.ttl(lifetime);
+        describe('ttl', function(){
+            it('should set timeToDeath', function(){
+                var lifetime = 3;
+                bullet.ttl(lifetime);
 
-		expect(bullet.ttl()).to.equal(lifetime);
-	    });
-	});
+                expect(bullet.ttl()).to.equal(lifetime);
+            });
+        });
 
-	describe('tick', function(){
-	    it('should update position', function(){
-		bullet.tick();
+        describe('tick', function(){
+            it('should update position', function(){
+                bullet.tick();
 
-		expect(bullet.position()).to.deep.equal({ x: 1, y: 0 });
-	    });
+                expect(bullet.position()).to.deep.equal({ x: 1, y: 0 });
+            });
 
-	    it('should die after a number of ticks', function(){
-		bullet.ttl(options.lifetime);
+            it('should die after a number of ticks', function(){
+                bullet.ttl(options.lifetime);
 
-		for (var index = 0; index < options.lifetime; index++) {
-		    bullet.tick();
-		}
+                for (var index = 0; index < options.lifetime; index++) {
+                    bullet.tick();
+                }
 
-		expect(bullet.ttl()).to.equal(0);
-	    });
+                expect(bullet.ttl()).to.equal(0);
+            });
 
-	    it('should notify when bullet dies', function(done){
-		bullet.addListener('died', done);
-		bullet.ttl(1);
+            it('should notify when bullet dies', function(done){
+                bullet.addListener('died', done);
+                bullet.ttl(1);
 
-		bullet.tick();
-	    });
-	});
+                bullet.tick();
+            });
+        });
     });
 });
